56c1aedbc8293141e12ce793e91bb316d3f5612d
Kenny <Kenny.graczyk2@gmail.com>
2022-09-23 02:14:19 +0900

    Add color selection, random color functions, edit overall look

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 const grid = document.querySelector("#grid");
 const resetButton = document.querySelector("#resetButton")
 const clearButton = document.querySelector("#clearButton")
+const randomColorButton = document.querySelector("#randomColorButton")
+const colorSelector = document.querySelector("#colorSelector")
+
+let resetLabel = document.querySelector("#resetLabel")
 let totalPixels = 16;
 let newPixels = 0;
+let mainColor = "rgb(26, 26, 26)"
+
+let randomColors = false;
 
 divCreator(totalPixels);
 
@@ -10,17 +17,33 @@ const container = document.querySelectorAll('.container');
 
 const innerSquare = document.querySelectorAll(".innerSquare")
 
-resetButton.addEventListener("click", reset);
+resetButton.addEventListener("input", reset);
 clearButton.addEventListener("click", clear);
+randomColorButton.addEventListener("click", switchColors);
+colorSelector.addEventListener("input", chooseColor);
+
+function chooseColor() {
+    mainColor = document.getElementById("colorSelector").value;
+}
+
+function switchColors() {
+    randomColors = !(randomColors);
+
+    if (randomColors === true) {
+        randomColorButton.classList.add('randomColorsActivated');  
+    } else if (randomColors === false) {
+        randomColorButton.classList.remove('randomColorsActivated');  
+    }
+};
 
 function reset() {
-    let userInput = parseInt(prompt("How many pixels would you like? (input will equal height and width)", 64));
-    if (userInput != null && (userInput > 0 && userInput < 101)) {
-        deleter();
-        divCreator(userInput);
-    } else {
-        alert("Please enter a number greater than 0 and less than 101.");
-    } 
+    let userInput = document.getElementById("resetButton").value;
+    deleter();
+    divCreator(userInput);
+
+    let horizontalPixels = Math.round(userInput * 1.5);
+
+    resetLabel.textContent = "Resize: " + userInput + " x " + horizontalPixels;
 };
 
 function divCreator(num) {
@@ -29,12 +52,25 @@ function divCreator(num) {
         div.setAttribute('class', 'container');                              
         grid.appendChild(div);
 
-        for (let x = 0; x < num; x++) {
+        for (let x = 0; x < num * 1.5; x++) {
             const divChildren = document.createElement('div');
             divChildren.setAttribute("class", "innerSquare");
             divChildren.addEventListener("mouseover", function () {
-                divChildren.classList.add('innerSquareHovered');   
-            })
+                if (randomColors === false) {
+                    divChildren.classList.add('innerSquareHovered');  
+                    divChildren.style.backgroundColor = mainColor
+                } else {
+                    divChildren.classList.add('innerSquareHoveredRandom');
+                    
+                    let rgbOne = Math.floor(Math.random() * 255) + 0;
+                    let rgbTwo = Math.floor(Math.random() * 255) + 0;
+                    let rgbThree = Math.floor(Math.random() * 255) + 0;
+
+                    let randomRGB = "rgb(" + rgbOne + ", " + rgbTwo + ", " + rgbThree + ")";
+
+                    divChildren.style.backgroundColor = randomRGB;
+                } 
+            }) 
             div.appendChild(divChildren);
         }
     }
@@ -49,5 +85,6 @@ function clear() {
     const newInnerSquare = document.querySelectorAll(".innerSquare")
     for (i of newInnerSquare) {
         i.classList.remove("innerSquareHovered");
+        i.style.backgroundColor = "rgb(255, 255, 255)";
     };
 }
